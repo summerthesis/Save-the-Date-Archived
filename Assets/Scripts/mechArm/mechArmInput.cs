9fa5db7bc3d8dfdd28858d190617b6e541f2546d
Hercules Dias Campos <hercules.amphibolite@gmail.com>
2019-11-13 18:16:24 -0500

    testing mechArm branch

---
+++ Assets/Scripts/mechArm/mechArmInput.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mechArmInput : MonoBehaviour
+{
+    [SerializeField] private float m_thunderDistance;
+    public float ThunderDistance { get { return m_thunderDistance; } }
+    [SerializeField] private float m_chargeRadius;
+    public float ChargeRadius { get { return m_chargeRadius; } }
+    [SerializeField] private int m_maxCharges;
+    public int MaxCharges { get { return m_maxCharges; } }
+    [SerializeField] private int m_charges; //FOR VISUALIZATION ONLY
+    public int Charges { get { return m_charges; } }
+    [SerializeField] private LineRenderer m_lr;
+
+    void Awake() {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Shock() {
+
+    }
+
+    void Discharge() { }
+
+    void Recharge() { }
+}
