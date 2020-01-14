b43eaee7e32380a403a56e2989ecefdd8eb36f7c
NikolaKordic <39223710+NikolaKordic@users.noreply.github.com>
2020-01-13 22:35:06 -0500

    Hook Mechanic update
    including GravityEnabled checkbox, and slight update of script otherwise
    Press R to reset variables during gameplay
    Press Space to enable hook (no longer automatic) - this will also draw a blue line from player to hook-point

--- Assets/Scripts/HookMech/HookMechanic.cs
+++ Assets/Scripts/HookMech/HookMechanic.cs
@@ -18,14 +18,27 @@ using UnityEngine;
 public class HookMechanic : MonoBehaviour
 {
     [SerializeField]
-    private float hookRadius; //collider radius
-    private SphereCollider hookSphereCollider; //collider component
+    private float hookRadius; 
+    //collider radius
+    private SphereCollider hookSphereCollider; 
+    //collider component
 
-    private bool pointInRange; //is the target in range?
-    private float distToHookPoint; // distance to hookpoint in sphere collider range
+    private bool pointInRange; 
+    //is the target in range?
+    private float distToHookPoint;
+    // distance to hookpoint in sphere collider range
+    private bool isHookEnabled = false;
+    //only hook after button has been pressed
 
     [SerializeField]
-    private float forceAmount; // force amount for push (approx 130 is at a point, where it still sends player forward)
+    private float forceAmount;
+    // force amount for push (approx 130 is at a point, where it still sends player forward)
+
+
+    [SerializeField]
+    private bool gravityDuringPull = false; 
+    //variable to determine if graivty should be enabled during hook(pull). 
+    // false will disable gravity. true will keep gravity enabled
 
     private Rigidbody rb; //player's rigid body component (to add force to, later on)
     private Collider nearestHookPoint;  //collider aspect of nearest point (for distance calculation)
@@ -55,26 +68,49 @@ public class HookMechanic : MonoBehaviour
         void OnTriggerExit(Collider other)
     {
         nearestHookPoint = null;
-        shortestDistance = new Vector3(15, 15, 15); 
-         Debug.Log("trigger exit"); 
+        shortestDistance = new Vector3(hookRadius, hookRadius, hookRadius);
+        Debug.Log("trigger exit"); 
     }
     // Update is called once per frame
     void Update()
     {
-        if (nearestHookPoint != null)
+        //checks to see if player has pressed hook button and if hook is within range
+        if (nearestHookPoint != null && isHookEnabled) 
         {
             Vector3 temp = nearestHookPoint.transform.position - this.transform.position; 
-
-            if (shortestDistance.magnitude > temp.magnitude)
+             Debug.DrawLine(this.transform.position, nearestHookPoint.transform.position, Color.red, 5.0f, true); 
+            if (shortestDistance.magnitude > temp.magnitude && shortestDistance.magnitude > 1) 
             {
-                shortestDistance = temp;
-            }else
+                if (gravityDuringPull == false)
+                {
+                    rb.useGravity = false; 
+                }
+                shortestDistance = temp; 
+                rb.AddForce((temp * forceAmount * Time.smoothDeltaTime)); 
+            }
+            else
             {
-                nearestHookPoint = null;
-                
+                rb.useGravity = true; 
+                isHookEnabled = false;
+                shortestDistance = new Vector3(hookRadius, hookRadius, hookRadius); 
             }
             //Debug.Log("trying to push!!" + temp);
-            rb.AddForce((temp * forceAmount * Time.smoothDeltaTime)); 
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.DrawLine(this.transform.position, nearestHookPoint.transform.position, Color.blue, 5.0f, true);
+            if (nearestHookPoint != null)
+            {
+                isHookEnabled = true; 
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            rb.useGravity = gravityDuringPull; 
+             //nearestHookPoint = null;
+             isHookEnabled = false;
+            shortestDistance = new Vector3(hookRadius, hookRadius, hookRadius); 
 
         }
     }
