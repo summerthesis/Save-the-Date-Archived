8b17bbda3f44568f5ea5010ee786b0e7c852b1c7
Comp0sator <Quarterinchspam@yahoo.com>
2020-01-17 11:02:35 -0500

    Added test scene with test mechanics

---
+++ Assets/Wiktor's Toybox (TEMP)/PushButton.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushButton : MonoBehaviour
+{
+    public Animator anim;
+
+    public bool isPressed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+        Debug.Log("pressed");
+        if(!isPressed && !isPlaying(anim, "PushButtonPressed"))
+        {
+            Debug.Log("Started");
+            anim.ResetTrigger("ButtonPressed");
+            isPressed = true;
+            anim.SetTrigger("ButtonPressed");
+        }
+    }
+
+    public bool isPlaying(Animator anim, string stateName)
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName(stateName) && anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+        {
+            return true;
+        }
+        else
+        {
+            isPressed = false;
+            return false;
+        }
+    }
+}
+
+
+////This gives you trigger like behavior from a bool
+//private IEnumerator AnimatorSetFire(float animationLength)
+//{
+//    animator.SetBool("Fire", true);
+
+//    //You can wait for seconds, frames, other coroutines, whatever u need 
+//    here
+//   yield return new WaitForSeconds(animationLength);
+
+//    animator.SetBool("Fire", false);
+//}
---
+++ Assets/Wiktor's Toybox (TEMP)/Turnstile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turnstile : MonoBehaviour
+{
+    public float RotateSpeed = 0.6f;
+    public bool isPowered = false;
+    public GameObject port;
+
+    int dir = 1;
+    Quaternion fromAngle;
+    Quaternion toAngle;
+    bool isRunning = false;
+
+    void Update()
+    {
+        //if(port.GetComponent.)
+    }
+
+    public void TurnWheel()
+    {
+        isPowered = !isPowered;
+
+        if (isPowered)
+        {
+            dir = -1;
+        }
+        else
+        {
+            dir = 1;
+        }
+
+        StartCoroutine(Turn(new Vector3(0, dir, 0) * 90, RotateSpeed));
+    }
+
+    IEnumerator Turn(Vector3 angle, float time)
+    {
+        fromAngle = transform.rotation;
+        toAngle = Quaternion.Euler(transform.eulerAngles + angle);
+        for (float t = 0f; t <= 1; t += Time.deltaTime / time)
+        {
+            transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+            yield return null;
+        }
+        transform.rotation = toAngle;
+    }
+}
