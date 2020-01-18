d7822da658f631bdf66b7e59dfce7dfebbc43c63
Comp0sator <Quarterinchspam@yahoo.com>
2020-01-17 22:02:17 -0500

    Updated Wiktor's Sandbox

---
+++ Assets/Wiktor's Toybox (TEMP)/Scripts/ElectricalPort.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricalPort : MonoBehaviour
+{
+    public bool isCharged = false;
+    public bool isActive = false;
+
+    void Start()
+    {
+        if (isCharged)
+            GetComponent<Renderer>().material.color = Color.red;
+        else
+            GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    void OnMouseDown()
+    {
+        if (!isActive)
+        {
+            isCharged = !isCharged;
+            isActive = !isActive;
+
+            if (isCharged)
+                GetComponent<Renderer>().material.color = Color.red;
+            else
+                GetComponent<Renderer>().material.color = Color.blue;
+        }
+    }
+}
