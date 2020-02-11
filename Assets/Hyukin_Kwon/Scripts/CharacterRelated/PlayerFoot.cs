b1c0ea85415f054915db75c4fd65a62e55b926ac
Hyukin Kown <901po3@gmail.com>
2020-02-11 13:49:12 -0500

    1

--- Assets/Hyukin_Kwon/Scripts/CharacterRelated/PlayerFoot.cs
+++ Assets/Hyukin_Kwon/Scripts/CharacterRelated/PlayerFoot.cs
@@ -1,27 +1,45 @@
-﻿using System.Collections;
+﻿/*********************************************************
+* Hyukin Kwon
+* Save The Date
+* 
+* PlayerFoot
+* Modified: 25 Jan 2020 - Hyukin
+* Last Modified: 11 Feb 2020 - Hyukin
+* 
+* Inherits from Monobehaviour
+* *******************************************************/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerFoot : MonoBehaviour
 {
     private bool m_bIsGrounded;
+    [SerializeField] float rayDis;
 
     public bool GetIsGrounded() { return m_bIsGrounded; }
     public void SetIsGrounded(bool b) { m_bIsGrounded = b; }
 
-    private void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        if (other.transform.tag != "Player" && !m_bIsGrounded)
-        {
-            m_bIsGrounded = true;
-        }
+        CheckGround();
     }
 
-    private void OnTriggerExit(Collider other)
+    private void CheckGround()
     {
-        if (other.transform.tag != "Player")
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(-Vector3.up), out hit, rayDis))
         {
-            m_bIsGrounded = false;
+            Debug.DrawRay(transform.position, transform.TransformDirection(-Vector3.up) * rayDis, Color.green);
+            if (hit.transform.tag != "Player")
+            {
+                m_bIsGrounded = true;
+            }
+
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(-Vector3.up) * rayDis, Color.red);
         }
     }
 }
--- Assets/Hyukin_Kwon/Scripts/CharacterRelated/PlayerMovement.cs
+++ Assets/Hyukin_Kwon/Scripts/CharacterRelated/PlayerMovement.cs
@@ -21,7 +21,7 @@ public class PlayerMovement : MonoBehaviour
 
     //** m_fMoveSpeed need to be roughly 0.12 of m_fSideMoveSpeed **
     [SerializeField] float m_fMoveSpeed;
-    [SerializeField] float m_fSideMoveSpeed;
+    private Vector3 m_fOverallSpeed = Vector3.zero;
     [SerializeField] float m_fRotSpeed;
 
     [SerializeField] bool m_bIsGrounded = true;
@@ -29,18 +29,21 @@ public class PlayerMovement : MonoBehaviour
 
     private float m_fHorizontal;
     private float m_fVertical;
+    private Quaternion qTo;
     private float fdt;
 
     private Rigidbody rigid;
 
     public float GetMoveSpeed() { return m_fMoveSpeed; }
-    public float GetSideMoveSpeed() { return m_fSideMoveSpeed; }
+
+    public Vector3 GetOverallSpeed() { return m_fOverallSpeed; }
 
     private void Start()
     {
         CameraBody = GameObject.Find("CameraBody");
         PlayerFoot = GameObject.Find("PlayerFoot").GetComponent<PlayerFoot>();
         rigid = GetComponent<Rigidbody>();
+        qTo = transform.rotation;
     }
 
     private void FixedUpdate()
@@ -49,7 +52,7 @@ public class PlayerMovement : MonoBehaviour
         m_fVertical = Input.GetAxis("Vertical");
         fdt = Time.fixedDeltaTime;
 
-        if ( CameraBehaviour.GetInstance().GetIsZooming())
+        if (CameraBehaviour.GetInstance().GetIsZooming())
         {
             ZoomInModeMove();
         }
@@ -63,8 +66,8 @@ public class PlayerMovement : MonoBehaviour
 
     private void JumpRegular()
     {
-        m_bIsGrounded = PlayerFoot.GetIsGrounded();
-        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Joystick1Button1)) && m_bIsGrounded)
+        //m_bIsGrounded = PlayerFoot.GetIsGrounded();
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Joystick1Button1)))//&& m_bIsGrounded)
         {
             Debug.Log("Jump");
             rigid.AddForce(Vector3.up * m_JumpForce);
@@ -73,7 +76,7 @@ public class PlayerMovement : MonoBehaviour
             if (m_fVertical != 0)
             {
                 rigid.AddForce(transform.forward * Mathf.Abs(m_fVertical) * m_JumpForce * 0.3f);
-            }     
+            }
             else if (m_fHorizontal != 0)
             {
                 rigid.AddForce(transform.forward * Mathf.Abs(m_fHorizontal) * m_JumpForce * 0.3f);
@@ -84,66 +87,66 @@ public class PlayerMovement : MonoBehaviour
     private void ZoomInModeMove()
     {
         if (!m_bIsGrounded) return;
-
-        if (m_fHorizontal > 0 && m_fVertical == 0)
-            transform.Translate(Vector3.right * m_fMoveSpeed * fdt, Space.Self);
-        else if(m_fHorizontal < 0 && m_fVertical == 0)
-            transform.Translate(-Vector3.right * m_fMoveSpeed * fdt, Space.Self);
-
-        if(m_fVertical > 0 && m_fHorizontal == 0)
-            transform.Translate(Vector3.forward * m_fMoveSpeed * fdt, Space.Self);
-        else if (m_fVertical < 0 && m_fHorizontal == 0)
-            transform.Translate(-Vector3.forward * m_fMoveSpeed * fdt, Space.Self);
+        Vector3 speed = Vector3.zero;
+        speed = (Vector3.right * m_fHorizontal) + (Vector3.forward * m_fVertical);
+        speed.Normalize();
+        speed *= m_fMoveSpeed;
+        transform.Translate(speed * fdt, Space.Self);
     }
 
     private void MoveRegular()
     {
-        if (!m_bIsGrounded) return;
+        //if (!m_bIsGrounded) return;
+
+        if (m_fVertical == 0 && m_fHorizontal != 0)
+            m_fOverallSpeed.x = m_fHorizontal * m_fMoveSpeed * 10;
+        else if (m_fVertical != 0 && m_fHorizontal == 0)
+            m_fOverallSpeed.z = Mathf.Abs(m_fVertical) * m_fMoveSpeed;
+        else if (m_fVertical != 0 && m_fHorizontal != 0)
+        {
+            m_fOverallSpeed.x = m_fHorizontal * m_fMoveSpeed * 2;
+            m_fOverallSpeed.z = Mathf.Abs(m_fVertical) * m_fMoveSpeed * 5;
+        }
+        m_fOverallSpeed = m_fOverallSpeed.normalized;
+        //Debug.Log(m_fOverallSpeed);
 
         if (m_fVertical != 0 && m_fHorizontal == 0)
-        {            
-            transform.Translate(Vector3.forward * m_fMoveSpeed * fdt, Space.Self);
-        }
-        if (m_fHorizontal > 0.2f && m_fVertical == 0)
         {
-            transform.RotateAround(CameraBody.transform.position, Vector3.up, m_fSideMoveSpeed * fdt);
+            transform.Translate(Vector3.forward * m_fOverallSpeed.z * m_fMoveSpeed * fdt, Space.Self);
         }
-        else if (m_fHorizontal < -0.2f && m_fVertical == 0)
+        if (m_fHorizontal != 0 && m_fVertical == 0)
         {
-            transform.RotateAround(CameraBody.transform.position, Vector3.up, -m_fSideMoveSpeed * fdt);
+            transform.RotateAround(CameraBody.transform.position, Vector3.up, m_fMoveSpeed * m_fOverallSpeed.x * 8 * fdt);
         }
-        else if (m_fVertical != 0 && m_fHorizontal > 0.2f)
+        else if (m_fVertical != 0 && m_fHorizontal != 0)
         {
-            transform.Translate(Vector3.forward * m_fMoveSpeed * 0.95f * fdt, Space.Self);
-            transform.RotateAround(CameraBody.transform.position, Vector3.up, m_fSideMoveSpeed * 0.03f * fdt);
-        }
-        else if (m_fVertical != 0 && m_fHorizontal < -0.2f)
-        {
-            transform.Translate(Vector3.forward * m_fMoveSpeed * 0.95f * fdt, Space.Self);
-            transform.RotateAround(CameraBody.transform.position, Vector3.up, -m_fSideMoveSpeed * 0.03f * fdt);
+            transform.Translate(Vector3.forward * m_fOverallSpeed.z * m_fMoveSpeed * fdt, Space.Self);
+            transform.RotateAround(CameraBody.transform.position, Vector3.up, m_fMoveSpeed * m_fOverallSpeed.x * 8 * fdt);
         }
     }
 
     private void PlayerFacingRot()
     {
-        if (!m_bIsGrounded) return;
+        //if (!m_bIsGrounded) return;
 
         if (m_fHorizontal > 0.2f && m_fVertical > 0.1f)
-            transform.rotation = Quaternion.LookRotation((CameraBody.transform.forward + CameraBody.transform.right).normalized * m_fRotSpeed * fdt);
+            qTo = Quaternion.LookRotation((CameraBody.transform.forward + CameraBody.transform.right).normalized);
         else if (m_fHorizontal > 0.2f && m_fVertical < -0.1f)
-            transform.rotation = Quaternion.LookRotation((-CameraBody.transform.forward + CameraBody.transform.right).normalized * m_fRotSpeed * fdt);
+            qTo = Quaternion.LookRotation((-CameraBody.transform.forward + CameraBody.transform.right).normalized);
         else if (m_fHorizontal < -0.2f && m_fVertical > 0.1f)
-            transform.rotation = Quaternion.LookRotation((CameraBody.transform.forward - CameraBody.transform.right).normalized * m_fRotSpeed * fdt);
+            qTo = Quaternion.LookRotation((CameraBody.transform.forward - CameraBody.transform.right).normalized);
         else if (m_fHorizontal < -0.2f && m_fVertical < -0.1f)
-            transform.rotation = Quaternion.LookRotation((-CameraBody.transform.forward - CameraBody.transform.right).normalized * m_fRotSpeed * fdt);
+            qTo = Quaternion.LookRotation((-CameraBody.transform.forward - CameraBody.transform.right).normalized);
         else if (m_fHorizontal > 0.2f && m_fVertical == 0)
-            transform.rotation = Quaternion.LookRotation(CameraBody.transform.right * m_fRotSpeed * fdt);
+            qTo = Quaternion.LookRotation(CameraBody.transform.right);
         else if (m_fHorizontal < -0.2f && m_fVertical == 0)
-            transform.rotation = Quaternion.LookRotation(-CameraBody.transform.right * m_fRotSpeed * fdt);
+            qTo = Quaternion.LookRotation(-CameraBody.transform.right);
         else if (m_fHorizontal == 0 && m_fVertical > 0.1f)
-            transform.rotation = Quaternion.LookRotation(CameraBody.transform.forward * m_fRotSpeed * fdt);
+            qTo = Quaternion.LookRotation(CameraBody.transform.forward);
         else if (m_fHorizontal == 0 && m_fVertical < -0.1f)
-            transform.rotation = Quaternion.LookRotation(-CameraBody.transform.forward * m_fRotSpeed * fdt);
+            qTo = Quaternion.LookRotation(-CameraBody.transform.forward);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, qTo, m_fRotSpeed * fdt);
     }
 
 }
