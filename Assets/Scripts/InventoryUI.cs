0420deb5f9fd19fa43a0377f556cf67501cd3094
Blair-White <blairjwhite@hotmail.com>
2020-02-23 11:24:29 -0500

    First draft main menu

--- Assets/Scripts/InventoryUI.cs
+++ Assets/Scripts/InventoryUI.cs
@@ -10,13 +10,21 @@ public class InventoryUI : MonoBehaviour
     public GameObject SampleButton2;
     public GameObject InventoryBox;
     public GameObject InvBackAnimPanel;
-    
+    public GameObject[] MainMenuCogs;
+    public GameObject[] MainMenuMemoirs;
+    public GameObject[] MemoirsPopUps;
+    public GameObject SelectInventory;
+    public GameObject SelectCharacter;
+    public GameObject[] Texts;
     enum State { closed = 0, Opening = 1, Closing = 2, Reserved = 3}
 
     private byte ColorTick = 0;
     
     public string state;
-    
+
+    private int timer1, timer2;
+    private float alpha0;
+    private Color color0, color1, color2;
     
     // Start is called before the first frame update
     void Start()
@@ -33,9 +41,31 @@ public class InventoryUI : MonoBehaviour
             FadePanel.GetComponent<Image>().color = new Color32(0, 0, 0, 0);
         }
         if (state == "Opening")
-        { 
-            ColorTick+=15;
-            state = ColorTick < 255 ? "Opening" : "Open";
+        {
+         
+            if (timer1 == 54) { SampleButton2.SetActive(true); alpha0 = 1.0f; }
+            alpha0+=0.015f;
+            SelectCharacter.GetComponent<Image>().color = new Color(100/255, 100/255, 100/255, alpha0);
+            SelectInventory.GetComponent<Image>().color = new Color(100/255, 100/255, 100/255, alpha0);
+            for (int i = 0; i < 15; i++)//todo get total number of all elements 
+            {
+               if(i < MainMenuCogs.Length)
+               {
+                MainMenuCogs[i].GetComponent<Image>().color = new Color(1, 1, 1, alpha0);
+               }
+               if (i < MainMenuMemoirs.Length)
+               {
+                MainMenuMemoirs[i].GetComponent<Image>().color = new Color(1, 1, 1, alpha0);
+               }
+               if (i < Texts.Length)
+               {
+                Texts[i].GetComponent<Text>().color = new Color(1, 1, 1, alpha0);
+               }
+            }
+        
+            if(ColorTick < 255) ColorTick+=15;
+            timer1++;
+            state = timer1 < 55 ? "Opening" : "Open";
             FadePanel.GetComponent<Image>().color = new Color32(0, 0, 0, ColorTick);
         }
         if (state == "Open")
@@ -44,11 +74,31 @@ public class InventoryUI : MonoBehaviour
         }
         if (state == "Closing")
         {
-            
-            if (ColorTick < 0) { ColorTick = 0;  }
-            FadePanel.GetComponent<Image>().color = new Color32(0, 0, 0, ColorTick);
+            if (timer1 == 14) { SampleButton.SetActive(true); alpha0 = 0.0f; }
+            alpha0 -= 0.115f;
+            SelectCharacter.GetComponent<Image>().color = new Color(100 / 255, 100 / 255, 100 / 255, alpha0);
+            SelectInventory.GetComponent<Image>().color = new Color(100 / 255, 100 / 255, 100 / 255, alpha0);
+            for (int i = 0; i < 15; i++)//todo get total number of all elements 
+            {
+                if (i < MainMenuCogs.Length)
+                {
+                    MainMenuCogs[i].GetComponent<Image>().color = new Color(1, 1, 1, alpha0);
+                }
+                if (i < MainMenuMemoirs.Length)
+                {
+                    MainMenuMemoirs[i].GetComponent<Image>().color = new Color(1, 1, 1, alpha0);
+                }
+                if (i < Texts.Length)
+                {
+                    Texts[i].GetComponent<Text>().color = new Color(1, 1, 1, alpha0);
+                }
+            }
             ColorTick -= 15;
-            state = ColorTick > 0 ? "Closing" : "Closed";
+            if (ColorTick < 0) { ColorTick = 0;  }
+            timer1++;
+            FadePanel.GetComponent<Image>().color = new Color32(0, 0, 0, ColorTick);
+          
+            state = timer1 < 15 ? "Closing" : "Closed";
         }
 
 
@@ -57,7 +107,8 @@ public class InventoryUI : MonoBehaviour
     public void Open()
     {
         InventoryBox.SetActive(true);
-        ColorTick = 0;  state = "Opening"; SampleButton.SetActive(false); SampleButton2.SetActive(true); 
+        alpha0 = 0; timer1 = 0; 
+        ColorTick = 0;  state = "Opening"; SampleButton.SetActive(false); SampleButton2.SetActive(false); 
         if(InvBackAnimPanel != null)
         {
         Animator animator = InvBackAnimPanel.GetComponent<Animator>();
@@ -73,7 +124,8 @@ public class InventoryUI : MonoBehaviour
     }
     public void Close() 
     {
-        ColorTick = 255;  state = "Closing"; SampleButton.SetActive(true); SampleButton2.SetActive(false); 
+        alpha0 = 1; timer1 = 0;
+        ColorTick = 255;  state = "Closing"; SampleButton.SetActive(false); SampleButton2.SetActive(false); 
         Animator animator = InvBackAnimPanel.GetComponent<Animator>();
         if(InvBackAnimPanel != null)
         {
