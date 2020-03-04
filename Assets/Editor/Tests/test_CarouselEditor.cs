01a2e6dff78f5b7940a201d375e6f8633d916e55
Hercules Dias Campos <hercules.amphibolite@gmail.com>
2020-03-04 16:19:36 -0500

    Drawing the platforms' initial locations. Draws a more detailed, non-interactive handle once the gameObject is selected. Also works with Prefabs.

--- Assets/Editor/Tests/test_CarouselEditor.cs
+++ Assets/Editor/Tests/test_CarouselEditor.cs
@@ -9,31 +9,57 @@ using static UnityEngine.Mathf; //THIS IS A DIRECTIVE, NOT A STATEMENT
 
 [CustomEditor(typeof(expendable_Carousel))]
 [CanEditMultipleObjects]
+
 public class test_CarouselEditor : Editor
 {
-    float size = 1;
-
+    float handleSize = 1;
+    float angle;
+    int numberOfPlatforms;
+    Transform transform;
+    float carouselRadius;
+    float angleDiv;
+    Vector3 platPosition;
     protected virtual void OnSceneGUI() {
-        if (Event.current.type == EventType.Repaint || Event.current.type == EventType.Layout) {
-            Transform transform = ((expendable_Carousel)target).transform;
-            int numberOfPlatforms = ((expendable_Carousel)target).NumberOfPlatforms;
-            float angle = 2 * PI / numberOfPlatforms;
-            float radius = ((expendable_Carousel)target).CarouselRadius;
+        if (Event.current.type == EventType.Repaint) {
+            transform = ((expendable_Carousel)target).transform;
+            numberOfPlatforms = ((expendable_Carousel)target).NumberOfPlatforms;
+            angle = numberOfPlatforms > 0 ? 2 * PI / numberOfPlatforms : 0 ;
+            carouselRadius = ((expendable_Carousel)target).CarouselRadius;
 
             Handles.color = Color.grey;
-            Handles.CubeHandleCap(0, transform.position, transform.rotation, size, EventType.Repaint);
+            Handles.CubeHandleCap(0, transform.position, transform.rotation, handleSize, EventType.Repaint);
             Handles.color = Color.white;
             for (int i = 0; i < numberOfPlatforms; ++i) {
-                float angleDiv = angle*i;
-                Vector3 offset = transform.TransformDirection(
-                    new Vector3(Mathf.Cos(angleDiv), 0,
-                                Mathf.Sin(angleDiv)));
-                Vector3 platPosition = transform.position + offset*radius;
-                Handles.SphereHandleCap(0, platPosition, transform.rotation, size, EventType.Repaint);
+                angleDiv = angle*i;
+                platPosition = CalculateInitialPosition(angleDiv);
                 Handles.DrawDottedLine(transform.position, platPosition, 10);
             }
             Handles.color = new Color(0.75f, 0.5f, 0.0f);
-            Handles.Disc(0, Quaternion.identity, transform.position, transform.up, radius, false, 0.01f);
+            Handles.Disc(0, Quaternion.identity, transform.position, transform.up, carouselRadius, false, 0.01f);
         }
+    }
+
+    #region Gizmo
+    [DrawGizmo(GizmoType.Selected | GizmoType.NonSelected | GizmoType.Active)]
+    static void DrawGizmosSelected(expendable_Carousel car, GizmoType gt)
+    {
+        float angle = car.NumberOfPlatforms > 0 ? 2*PI/car.NumberOfPlatforms : 0;
+        float angleDiv;
+        Vector3 targetPos;
+        for (int i = 0; i < car.NumberOfPlatforms; ++i)
+        {
+            angleDiv = i * angle;
+            targetPos = car.CalculateInitialPosition(angleDiv);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(targetPos, 1);
+            //Handles.Label(targetPos, "Platform");
+        }
+    }
+    #endregion
+
+    private Vector3 CalculateInitialPosition(float angle) {
+        return transform.position + transform.TransformDirection(
+                    new Vector3(Mathf.Cos(angleDiv), 0, Mathf.Sin(angleDiv)))*
+                    carouselRadius ;
     }
 }
--- Assets/Scripts/HErC/Expendables/expendable_Carousel.cs
+++ Assets/Scripts/HErC/Expendables/expendable_Carousel.cs
@@ -8,8 +8,9 @@ public class expendable_Carousel : MonoBehaviour
     [SerializeField] private float m_fRotationSpeed;
     [SerializeField] private float m_fCarouselRadius;
     [SerializeField] private float m_fPlatformSize;
-    [SerializeField] private bool m_bShouldLookAtCenter;
+    [SerializeField] private bool m_bShouldLookAtCenter;//takes precedence
     [SerializeField] private bool m_bShouldLookAwayFromCenter;
+    [SerializeField] private GameObject m_PlatformPrefab;
     [SerializeField] private GameObject[] m_Platforms;
 
     float angle;
@@ -21,45 +22,50 @@ public class expendable_Carousel : MonoBehaviour
     public float PlatformSize { get { return m_fPlatformSize; } }
 
     #endregion
-
+        
     void Awake() {
 
         angle = m_Platforms.Length > 0 ? 2 * Mathf.PI / m_Platforms.Length : 0;
         for  (int i = 0; i < m_Platforms.Length; ++i) {
             angleDiv = i * angle;
-            m_Platforms[i] = GameObject.CreatePrimitive(m_pTypeToCreate);
-            m_Platforms[i].AddComponent<expendable_PlatformParenting>();
-            m_Platforms[i].transform.parent = transform;
-            m_Platforms[i].transform.position = this.gameObject.transform.position + 
-                transform.TransformDirection(
-                    new Vector3(Mathf.Cos(angleDiv), 0,
-                                Mathf.Sin(angleDiv)))*
-                m_fCarouselRadius;
-            switch (m_pTypeToCreate) {
-                case PrimitiveType.Sphere:
-                case PrimitiveType.Capsule:
-                case PrimitiveType.Plane:
-                case PrimitiveType.Quad:
-                    m_Platforms[i].transform.localScale = Vector3.one * m_fPlatformSize;
-                    break;
-                case PrimitiveType.Cylinder:
-                    Destroy(m_Platforms[i].GetComponent<Collider>());
-                    m_Platforms[i].transform.localScale = new Vector3(m_fPlatformSize, 0.5f, m_fPlatformSize);
-                    m_Platforms[i].AddComponent<BoxCollider>();
-                    break;
-                case PrimitiveType.Cube:
-                    m_Platforms[i].transform.localScale = new Vector3(m_fPlatformSize, 1, m_fPlatformSize);
-                    break;
-                default:
-                    break;
+            if (m_PlatformPrefab)
+            {
+                m_Platforms[i] = GameObject.Instantiate(m_PlatformPrefab, //template
+                                                        CalculateInitialPosition(angleDiv), //position
+                                                        Quaternion.identity, this.gameObject.transform); //rotation/parent
             }
+            else {
+                m_Platforms[i] = GameObject.CreatePrimitive(m_pTypeToCreate);
+                m_Platforms[i].transform.parent = transform;
+                m_Platforms[i].transform.position = CalculateInitialPosition(angleDiv);
+
+                switch (m_pTypeToCreate) {
+                    case PrimitiveType.Sphere:
+                    case PrimitiveType.Capsule:
+                    case PrimitiveType.Plane:
+                    case PrimitiveType.Quad:
+                        m_Platforms[i].transform.localScale = Vector3.one * m_fPlatformSize;
+                        break;
+                    case PrimitiveType.Cylinder:
+                        Destroy(m_Platforms[i].GetComponent<Collider>());
+                        m_Platforms[i].transform.localScale = new Vector3(m_fPlatformSize, 0.5f, m_fPlatformSize);
+                        m_Platforms[i].AddComponent<BoxCollider>();
+                        break;
+                    case PrimitiveType.Cube:
+                        m_Platforms[i].transform.localScale = new Vector3(m_fPlatformSize, 1, m_fPlatformSize);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            m_Platforms[i].AddComponent<expendable_PlatformParenting>();
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(transform.position, transform.up, m_fRotationSpeed);
+        transform.RotateAround(transform.position, transform.up, m_fRotationSpeed*Time.deltaTime);
 
         if (m_pTypeToCreate != PrimitiveType.Quad) {
             for (int i = 0; i < m_Platforms.Length; ++i) {
@@ -86,5 +92,17 @@ public class expendable_Carousel : MonoBehaviour
                 g.transform.Rotate(this.gameObject.transform.up, 180, Space.Self);
             }
         }
+    }
+
+
+    /// <summary>
+    /// Calculates the platform's initial position based on the parent's initial position and the 
+    /// angle returned by the number of subdivisions
+    /// </summary>
+    /// <param name="angle"></param>
+    /// <returns></returns>
+    public Vector3 CalculateInitialPosition(float angle) {
+        return (this.gameObject.transform.position + 
+            transform.TransformDirection( new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle))) * m_fCarouselRadius);
     }
 }
