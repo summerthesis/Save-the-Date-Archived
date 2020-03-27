ea4531b00a0dac12d68691a169a859a80231e5c1
Comp0sator <Quarterinchspam@yahoo.com>
2020-03-27 18:37:17 -0400

    Archiving Project
    All items in the project have been properly profiled and archived.

---
+++ Assets/Wiktor's Toybox/Scripts/ElectricalPort.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricalPort : MonoBehaviour
+{
+    private Chargeable chargeable;
+    public bool isCharged { get { return chargeable.Charged; } }
+    private bool hasCharged;
+    private bool startedRunning;
+    public bool Active { get { return hasCharged ^ startedRunning; } }
+
+    void Start()
+    {
+        startedRunning = false;
+        chargeable = this.gameObject.GetComponent<Chargeable>();
+        GetComponent<Renderer>().material.color = chargeable.Charged ? Color.red : Color.blue;
+        hasCharged = chargeable.Charged;
+    }
+
+    void Update()
+    {
+        hasCharged = chargeable.Charged;
+        if (chargeable) GetComponent<Renderer>().material.color = chargeable.Charged ? Color.red : Color.blue;
+        else GetComponent<Renderer>().material.color = Color.blue;
+
+        if (Active) { this.gameObject.layer = 0; }
+        else { this.gameObject.layer = 8; } //LAYER 8 is ThunderPhysics
+    }
+
+    public void SetActiveState(bool activeState)
+    {
+        startedRunning = activeState;
+    }
+}
---
+++ Assets/Wiktor's Toybox/Scripts/Turnstile.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turnstile : MonoBehaviour
+{
+    public float RotateSpeed = 0.6f;
+    public bool isPowered = false;
+    public bool isPort;
+    public GameObject port;
+
+    int dir = 1;
+    Quaternion fromAngle;
+    Quaternion toAngle;
+    bool isRunning = false;
+
+    /// <summary>
+    /// HErC's observation: I changed the tracking system of the Port:
+    /// if it's charged but not active, it activates.
+    /// After the activation ends, it deactivates
+    /// </summary>
+    void Update()
+    {
+        if (!isPort)
+        {
+            if (port.GetComponentInChildren<PushButton>().isActive && !isRunning)
+            {
+                isRunning = true;
+                TurnWheel();
+            }
+        }
+
+        if (isPort)
+        {
+            if (port.GetComponentInChildren<ElectricalPort>().Active && !isRunning)
+            {
+                isRunning = true;
+                TurnWheel();
+            }
+        }
+    }
+
+    public void TurnWheel()
+    {
+        isPowered = !isPowered;
+
+        if (isPowered)
+        {
+            dir = -1;
+        }
+        else
+        {
+            dir = 1;
+        }
+
+        StartCoroutine(Turn(new Vector3(0, dir, 0) * 90, RotateSpeed));
+    }
+
+    IEnumerator Turn(Vector3 angle, float time)
+    {
+        fromAngle = transform.rotation;
+        toAngle = Quaternion.Euler(transform.eulerAngles + angle);
+        for (float t = 0f; t <= 1; t += Time.deltaTime / time)
+        {
+            transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+            yield return null;
+        }
+        transform.rotation = toAngle;
+        isRunning = false;
+
+        if (!isPort)
+        {
+            port.GetComponentInChildren<PushButton>().isActive = false;
+        }
+        else
+        {
+            port.GetComponentInChildren<ElectricalPort>().SetActiveState(port.GetComponentInChildren<ElectricalPort>().isCharged);
+        }
+    }
+}
